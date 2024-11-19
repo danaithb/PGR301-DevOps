eb83d693ee3841dbdbcf43f2b1f6c9ed91bd8abc
EC2 Default User <ec2-user@ip-172-31-43-211.eu-west-1.compute.internal>
2024-11-19 14:48:38 +0000

    2b - Terraform workflow test

--- infra/sqs.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_sqs_queue" "image_queue" {
-  name = "image_generation_queue_104"
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,19 @@
-variable "candidate_number" {
-  default = "104"
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+  default     = "104"
 }
+
+variable "bucket_name" {
+  description = "S3 bucket for storing generated images"
+  type        = string
+  default     = "pgr301-couch-explorers"
+}
+
+variable "sqs_queue_name" {
+  description = "Name of SQS queue for image generation"
+  type        = string
+  default     = "image_generation_queue"
+}
+
+#testing terraform workflow
